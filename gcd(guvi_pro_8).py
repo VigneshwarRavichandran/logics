e759ceed98a5de031ef67d1b89c9336f73e54272
Vigneshwar <vigneshwarravichandran@gmail.com>
2019-01-29 19:18:36 +0530

    Update gcd(guvi_pro_8).py

--- gcd(guvi_pro_8).py
+++ gcd(guvi_pro_8).py
@@ -5,12 +5,12 @@ q=ip1[1]
 arr=list(map(int,input().split()))
 for i in range(0,q):
     ip2=list(map(int,input().split()))
-    l=ip2[0]
-    r=ip2[1]
-    if(l==r):
-        temp_res=math.gcd(l,r)
-        res=temp_res*arr[0]
-        print(res)
+    l=ip2[0]-1
+    r=ip2[1]-1
+    if(l>len(arr)):
+        print(arr(r))
+    elif(r>len(arr)):
+        print(arr(l))
     else:
-        res=math.gcd(l,r)
+        res=math.gcd(arr[l],arr[r])
         print(res)
