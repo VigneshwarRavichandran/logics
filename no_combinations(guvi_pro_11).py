fdd69ff050498e400345e9de4edc5b3154961ebf
Vigneshwar <vigneshwarravichandran@gmail.com>
2019-01-31 23:26:24 +0530

    Update no_combinations(guvi_pro_11).py

--- no_combinations(guvi_pro_11).py
+++ no_combinations(guvi_pro_11).py
@@ -1,6 +1,6 @@
 n=int(input())
-count=0
+count=[]
 for i in range(1,n):
     for j in range(i+1,n+1):
-        count=count+1
-print(count)      
+        count.append([i,j])
+print(len(count))       
